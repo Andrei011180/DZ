b1d89f362b66995e8202bad54ad3ee35daa85b8a
Andrei <reysavin@yandex.ru>
2023-10-11 03:34:30 +1000

    Program.cs

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿/*Напишите программу , которая на вход принимает трехзначное число , 
+а на выходе показывает вторую цифру этого числа
+*/
+// Console.WriteLine("Enter the third chair number ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int new_num = (number / 10) %10; 
+// {
+//  if ( number<100 && number>999)
+//    Console.WriteLine ("Error");
+ 
+//  else  
+//  Console.WriteLine($"second chair is {new_num}");
+//  }
+ 
+
+
+/* Напишите программу , которая выводит третью цифру заданного числа 
+ или сообщает , что третьей цифры нет .
+*/
+
+// Console.WriteLine(" Enter the number ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+//    if ( number<99)
+//    {
+//    Console.WriteLine( " There is no third digit ");
+//    }
+//     else 
+//     {
+//         number=number/100;
+//         number=number%10
+//         Console.WriteLine($"Third digit is{number}");
+
+
+/*Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+и проверяет, является ли этот день выходным
+*/
+
+// Console.WriteLine(" Enter the number ");
+// int number = Convert.ToInt32(Console.ReadLine());
+//  if( number <0 || number >7)
+//      {
+//        Console.WriteLine( "Error");
+//      }
+//          if (number >0 || number <6)
+//         {
+//              Console.WriteLine("Not WeekEnd");
+//         }
+//            else if (number>6 || number<7);
+//            {
+//                  Console.WriteLine("WeekEnd");
+//            }
